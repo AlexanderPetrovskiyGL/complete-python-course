99ca8f609a4b3ebb17c48332e4e735b33c8e896b
Alexander Petrovskiy <alexander.petrovskiy@gmail.com>
2025-07-15 20:01:29 +0300

    Added calc exersice

--- course_contents/6_files/csv2json/app.py
+++ course_contents/6_files/csv2json/app.py
@@ -1,13 +1,11 @@
 import json
-
-with open('csv_file.txt', 'r') as file:
-    file_records = [line.strip() for line in file.readlines()]
+import csv
 
 json_object: list[dict] = []
 
-for record in file_records:
-    club_value, city_value, country_value  = record.split(',')
-    json_object.append({'club': club_value, 'city': city_value, 'country': country_value})
+with open('csv_file.txt', 'r') as file:
+    for row in csv.DictReader(file, fieldnames=['club', 'city', 'country']):
+        json_object.append(row)
 
 with open('json_file.txt', 'w') as file:
     json.dump(json_object, file)
